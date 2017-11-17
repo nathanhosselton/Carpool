2a10eb4d09daac4acba2b62519d5c883af6d3f56
Nathan Hosselton <nathanhosselton@gmail.com>
2017-11-17 11:29:28 -0500

    Flesh out StackView a little more

--- Sources/etc.swift
+++ Sources/etc.swift
@@ -5,11 +5,22 @@ class StackView<T: UIView>: UIStackView {
         self.init(arrangedSubviews: arrangedSubviews as [UIView])
     }
 
+    override func addArrangedSubview(_ view: UIView) {
+        guard view is T else { fatalError("Expected view of type \(String(describing: T.self)) but got \(String(describing: type(of: view)))") }
+        super.addArrangedSubview(view)
+    }
+
+    override func insertArrangedSubview(_ view: UIView, at stackIndex: Int) {
+        guard view is T else { fatalError("Expected view of type \(String(describing: T.self)) but got \(String(describing: type(of: view)))") }
+        super.insertArrangedSubview(view, at: stackIndex)
+    }
+
     var views: [T] {
-        return subviews as! [T]
+        return arrangedSubviews as! [T]
     }
 
     subscript(i: Int) -> T {
         get { return views[i] }
+        set { insertArrangedSubview(newValue, at: i) }
     }
 }
